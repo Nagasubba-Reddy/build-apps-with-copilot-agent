0cb2bb49e1746ad8ddea0b9d0bd351bef878d969
Nagasubba-Reddy <nagasubbareddy.m@gmail.com>
2026-09-14 12:02:34 +0530

    Fix typo in main.jsx - correct createCRoot to createRoot

--- octofit-tracker/frontend/src/main.jsx
+++ octofit-tracker/frontend/src/main.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
 
-createCRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
